0fab0386332179689d0f2754de5974e659cc3066
Vitor Nunes <stecks10@gmail.com>
2021-06-22 11:39:23 -0300

    Aula 2 nlw

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
-import './services/firebase'
+import './services/firebase';
+
+import './styles/global.scss';
 
 ReactDom.render(
   <React.StrictMode>
