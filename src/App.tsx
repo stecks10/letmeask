8d29e439f0ec37850accb98e6c95c870edb24f26
Vitor Nunes <stecks10@gmail.com>
2021-07-30 18:47:41 -0300

    Aula 02 - Maximum Speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
-import { NewRoom } from "./pages/NewRoom";
+import { NewRoom } from './pages/NewRoom';
 
-import { AuthContextProvider } from './contexts/AuthContext'
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
-    return (
+  return (
     <BrowserRouter>
       <AuthContextProvider>
         <Route path="/" exact component={Home} />
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,8 +1,7 @@
 import { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
-
+import { AuthContext } from '../context/AuthContext';
 export function useAuth() {
-  const value = useContext(AuthContext)
+  const value = useContext(AuthContext);
 
   return value;
 }
